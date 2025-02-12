abd545423b294333f52f6587db6bbfb01e0f801b
Israel Teles Bandeira <44622353+sr-bauducco@users.noreply.github.com>
2025-02-12 16:13:02 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserViewSet, ProdutoViewSet, FeiraViewSet, BancaViewSet, AvaliacaoViewSet
 
 # Criando o roteador para registrar os viewsets
@@ -11,5 +12,8 @@ router.register(r'bancas', BancaViewSet)
 router.register(r'avaliacoes', AvaliacaoViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('produtos.urls')),
 ]
